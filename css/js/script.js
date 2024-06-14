79ef8a1c6d15b74f09da3a68d1ddfa5f69123a55
Sarah Acquaye Amedior <amediorsarah@gmail.com>
2024-06-14 13:07:10 +0000

    third commit

--- css/js/script.js
+++ css/js/script.js
@@ -1,16 +1,9 @@
-// var slideIndex = 0;
-// showSlides();
+const btn= document.getElementById('btn')
+btn.onclick = function(){
+    alert('Well Recieved')
+}
 
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("slides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+const fbtn= document.getElementById('fbtn')
+fbtn.onclick = function(){
+    alert('Well Recieved')
+}
